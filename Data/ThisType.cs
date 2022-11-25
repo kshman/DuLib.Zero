55a7d902d9b0a5b4cf106fbbd36fe5011a8ba286
kim <kshman@msn.com>
2022-11-25 20:20:02 +0900

    기능 추가 (바닥 버튼, 이 타입 )

---
+++ Data/ThisType.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+namespace Du.Data;
+
+/// <summary>
+/// this에 대한 조작 기능
+/// </summary>
+public static class ThisType
+{
+	/// <summary>
+	/// 타입의 어트리뷰트를 가져온다
+	/// </summary>
+	/// <typeparam name="TA"></typeparam>
+	/// <typeparam name="TT"></typeparam>
+	/// <param name="type"></param>
+	/// <returns></returns>
+	public static TA? GetAttribute<TA, TT>(this TT type)
+		where TA : Attribute
+		where TT : class, Enum
+	{
+		var fld = type.GetType().GetField(type.ToString());
+		var attr = fld?.GetCustomAttributes(typeof(TA), false) as TA[];
+		return attr?.FirstOrDefault();
+	}
+
+	/// <summary>
+	/// 타입에서 설명 어트리뷰트를 가져온다
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="type"></param>
+	/// <returns></returns>
+	public static string GetDescription<T>(this T type) where T : class, Enum
+	{
+		var attr = type.GetAttribute<DescriptionAttribute, T>();
+		var desc = attr?.Description;
+		return desc ?? type.ToString();
+	}
+
+	/// <summary>
+	/// 열거형에서 설명 어트리뷰트를 가져온다
+	/// </summary>
+	/// <param name="e"></param>
+	/// <returns></returns>
+	public static string GetDescription(this Enum e)
+	{
+		return GetDescription<Enum>(e);
+	}
+
+	/// <summary>
+	/// 예외에서 InnerException이나 그냥 메시지를 가져온다
+	/// </summary>
+	/// <param name="exception"></param>
+	/// <returns></returns>
+	public static string GetMessage(this Exception exception)
+		=> exception.InnerException?.Message ?? exception.Message;
+}
