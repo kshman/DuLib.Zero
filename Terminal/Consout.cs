031b19311a2ce3ad20adc82e008340b9eaec72a9
SANGHOI KIM <kshman@msn.com>
2022-12-17 21:25:41 +0900

    Zero/ModBus 수정 * Consout 추가 * TermAnsi 추가 * 모드버스 클라이언트 종료 때 강제 타임아웃처리

---
+++ Terminal/Consout.cs
@@ -0,0 +1,212 @@
+﻿namespace Du.Terminal;
+
+/// <summary>
+/// 콘솔 아웃풋 전용
+/// </summary>
+public static class Consout
+{
+	#region 잠금
+	private static readonly object s_lock = new();
+
+	/// <summary>
+	/// 클래스 잠금 오브젝트
+	/// </summary>
+	public static object LockObject => s_lock;
+	#endregion
+
+	#region 출력
+	/// <summary>
+	/// 시간 색깔
+	/// </summary>
+	public static string TimedTermColor { get; set; } = TermAnsi.Yellow;
+	/// <summary>
+	/// 열기 브라켓
+	/// </summary>
+	public static string OpenBracket { get; set; } = "[";
+	/// <summary>
+	/// 닫기 브라켓
+	/// </summary>
+	public static string CloseBracket { get; set; } = "]";
+
+	/// <summary>
+	/// 콘솔 출력
+	/// </summary>
+	/// <param name="message"></param>
+	public static void Write(string message)
+	{
+		lock (s_lock)
+			Console.Write(message);
+	}
+
+	/// <summary>
+	/// 콘솔 출력 + 새줄
+	/// </summary>
+	/// <param name="message"></param>
+	public static void WriteLine(string message)
+	{
+		lock (s_lock)
+			Console.WriteLine(message);
+	}
+
+	/// <summary>
+	/// 콘솔 새줄
+	/// </summary>
+	public static void WriteLine()
+	{
+		lock (s_lock)
+			Console.WriteLine();
+	}
+
+	/// <summary>
+	/// 콘솔 출력: 시간 + 메시지
+	/// </summary>
+	/// <param name="message"></param>
+	public static void TimedWrite(string message)
+	{
+		lock (s_lock)
+			Console.Write($"{OpenBracket}{TimedTermColor}{DateTime.Now:O}{TermAnsi.Reset}{CloseBracket} {message}");
+	}
+
+	/// <summary>
+	/// 콘솔 출력 + 새줄: 시간 + 메시지
+	/// </summary>
+	/// <param name="message"></param>
+	public static void TimedWriteLine(string message)
+	{
+		lock (s_lock)
+			Console.WriteLine($"{OpenBracket}{TimedTermColor}{DateTime.Now:O}{TermAnsi.Reset}{CloseBracket} {message}");
+	}
+	#endregion
+
+	#region 탑 텍스트
+	/// <summary>
+	/// 리포트 활성 색깔
+	/// </summary>
+	public static string ReportActive { get; set; } = $"{TermAnsi.Yellow}{TermAnsi.BgDarkCyan}";
+	/// <summary>
+	/// 리포트 남은 색깔
+	/// </summary>
+	public static string ReportLeft { get; set; } = $"{TermAnsi.Gray}{TermAnsi.BgDarkGray}";
+	/// <summary>
+	/// 리포트 위치
+	/// 이 값이 0보다 작으면 화면 아래부터 시작한다
+	/// </summary>
+	public static int ReportLocation { get; set; } = 0;
+	/// <summary>
+	/// 리포트에 퍼센트를 붙여요
+	/// </summary>
+	public static bool ReportShowPercent { get; set; } = true;
+
+	/// <summary>
+	/// 진행바 표시
+	/// </summary>
+	/// <param name="value"></param>
+	/// <param name="message"></param>
+	public static void Report(double value, string? message = null) =>
+		InternalReport(value, message ?? string.Empty);
+
+	/// <summary>
+	/// 진행바 표시
+	/// </summary>
+	/// <param name="value"></param>
+	/// <param name="max"></param>
+	/// <param name="message"></param>
+	public static void Report(int value, int max, string? message = null) =>
+		InternalReport((double)value / max, message ?? string.Empty);
+
+	//
+	private static void InternalReport(double value, string message)
+	{
+		value = Math.Clamp(value, 0, 1);
+		StringBuilder sb;
+
+		if (Console.IsOutputRedirected)
+		{
+			if (!ReportShowPercent)
+				WriteLine(message);
+			else
+			{
+				sb = new StringBuilder(message);
+				sb.Append((int)(value * 100)).Append('%');
+				WriteLine(sb.ToString());
+			}
+			return;
+		}
+
+		var width = Console.WindowWidth;
+		sb = new StringBuilder(message.Length < width ? message : message[..width]); // 2바이트 문자는... 계산 안됨
+
+		var progress = Math.Clamp((int)(value * width), 0, width);
+		var (length, left) = InternalStringLength(message, progress); // 2바이트 문자 계산용
+
+		if (!ReportShowPercent)
+		{
+			if (width > length)
+				sb.Append(' ', width - length);
+		}
+		else
+		{
+			if (width > length - 5)
+			{
+				sb.Append(' ', width - length - 5);
+				sb.AppendFormat("{0,3}% ", (int)(value * 100));
+			}
+			else
+			{
+				// 아니 이건 %붙일 자리가 없네
+				if (width > length)
+					sb.Append(' ', width - length);
+			}
+		}
+
+		sb.Insert(progress - left, ReportLeft);
+		sb.Insert(0, ReportActive);
+		sb.Append(TermAnsi.Reset);
+
+		lock (s_lock)
+		{
+			Console.CursorVisible = false;
+			var (sx, sy) = Console.GetCursorPosition();
+
+			Console.SetCursorPosition(0, InternalRealReportLocation);
+			Console.Write(sb.ToString());
+
+			Console.SetCursorPosition(sx, sy);
+			Console.CursorVisible = true;
+		}
+	}
+
+	//
+	private static (int len, int left) InternalStringLength(string input, int progress)
+	{
+		var left = 0;
+		var len = 0;
+		for (var i = 0; i < input.Length; i++)
+		{
+			if (char.IsAscii(input[i]))
+				len++;
+			else
+			{
+				len += 2;
+				if (i < progress)
+					left++;
+			}
+		}
+		return (len, left);
+	}
+
+	//
+	private static int InternalRealReportLocation
+	{
+		get
+		{
+			if (ReportLocation >= 0)
+				return ReportLocation;
+
+			int height = Console.WindowHeight;
+			return height + ReportLocation;
+		}
+	}
+	#endregion
+}
+
---
+++ Terminal/TermAnsi.cs
@@ -0,0 +1,161 @@
+﻿namespace Du.Terminal;
+
+/// <summary>
+/// 콘솔용 안시 코드
+/// </summary>
+public static class TermAnsi
+{
+	#region 상태
+	/// <summary>
+	/// 상태 리셋
+	/// </summary>
+	public const string Reset = "\x1b[0m";
+	/// <summary>
+	/// 두껍게
+	/// </summary>
+	public const string Bold = "\x1b[1m";
+	/// <summary>
+	/// 밑줄
+	/// </summary>
+	public const string Underline = "\x1b[4m";
+	/// <summary>
+	/// 반전
+	/// </summary>
+	public const string Reverse = "\x1b[7m";
+	#endregion
+
+	#region 글자색
+	/// <summary>
+	/// 검정
+	/// </summary>
+	public const string Black = "\x1b[30m";
+	/// <summary>
+	/// 빨강
+	/// </summary>
+	public const string DarkRed = "\x1b[31m";
+	/// <summary>
+	/// 초록
+	/// </summary>
+	public const string DarkGreen = "\x1b[32m";
+	/// <summary>
+	/// 노랑
+	/// </summary>
+	public const string DarkYellow = "\x1b[33m";
+	/// <summary>
+	/// 파랑
+	/// </summary>
+	public const string DarkBlue = "\x1b[34m";
+	/// <summary>
+	/// 자홍
+	/// </summary>
+	public const string DarkMagenta = "\x1b[35m";
+	/// <summary>
+	/// 청록
+	/// </summary>
+	public const string DarkCyan = "\x1b[36m";
+	/// <summary>
+	/// 회색
+	/// </summary>
+	public const string Gray = "\x1b[37m";
+	/// <summary>
+	/// 밝은 회색
+	/// </summary>
+	public const string DarkGray = "\x1b[90m";
+	/// <summary>
+	/// 밝은 빨강
+	/// </summary>
+	public const string Red = "\x1b[91m";
+	/// <summary>
+	/// 밝은 초록
+	/// </summary>
+	public const string Green = "\x1b[92m";
+	/// <summary>
+	/// 밝은 노랑
+	/// </summary>
+	public const string Yellow = "\x1b[93m";
+	/// <summary>
+	/// 밝은 파랑
+	/// </summary>
+	public const string Blue = "\x1b[94m";
+	/// <summary>
+	/// 밝은 자홍
+	/// </summary>
+	public const string Magenta = "\x1b[95m";
+	/// <summary>
+	/// 밝은 청록
+	/// </summary>
+	public const string Cyan = "\x1b[96m";
+	/// <summary>
+	/// 하양
+	/// </summary>
+	public const string White = "\x1b[97m";
+	#endregion
+
+	#region 배경색
+	/// <summary>
+	/// 검정
+	/// </summary>
+	public const string BgBlack = "\x1b[40m";
+	/// <summary>
+	/// 빨강
+	/// </summary>
+	public const string BgDarkRed = "\x1b[41m";
+	/// <summary>
+	/// 초록
+	/// </summary>
+	public const string BgDarkGreen = "\x1b[42m";
+	/// <summary>
+	/// 노랑
+	/// </summary>
+	public const string BgDarkYellow = "\x1b[43m";
+	/// <summary>
+	/// 파랑
+	/// </summary>
+	public const string BgDarkBlue = "\x1b[44m";
+	/// <summary>
+	/// 자홍
+	/// </summary>
+	public const string BgDarkMagenta = "\x1b[45m";
+	/// <summary>
+	/// 청록
+	/// </summary>
+	public const string BgDarkCyan = "\x1b[46m";
+	/// <summary>
+	/// 하양
+	/// </summary>
+	public const string BgGray = "\x1b[47m";
+	/// <summary>
+	/// 검정
+	/// </summary>
+	public const string BgDarkGray = "\x1b[100m";
+	/// <summary>
+	/// 빨강
+	/// </summary>
+	public const string BgRed = "\x1b[101m";
+	/// <summary>
+	/// 초록
+	/// </summary>
+	public const string BgGreen = "\x1b[102m";
+	/// <summary>
+	/// 노랑
+	/// </summary>
+	public const string BgYellow = "\x1b[103m";
+	/// <summary>
+	/// 파랑
+	/// </summary>
+	public const string BgBlue = "\x1b[104m";
+	/// <summary>
+	/// 자홍
+	/// </summary>
+	public const string BgMagenta = "\x1b[105m";
+	/// <summary>
+	/// 청록
+	/// </summary>
+	public const string BgCyan = "\x1b[106m";
+	/// <summary>
+	/// 하양
+	/// </summary>
+	public const string BgWhite = "\x1b[107m";
+	#endregion
+}
+
